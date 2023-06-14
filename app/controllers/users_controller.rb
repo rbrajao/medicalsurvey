cc99a6a7e3a1423e6c4ab31bd6c9b56acfc75b7b
Rafael <rbrajao@gmail.com>
2023-06-14 16:41:00 -0300

    questions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+    before_action :authenticate_user!
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path, notice: 'User created successfully.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if params[:user][:encrypted_password].blank?
+      @user.update_without_password(user_params)
+      render :edit
+    else
+        @user.password = params[:user][:encrypted_password]
+        @user.save(validate: false) # Salva as alterações sem validar a senha atual
+        redirect_to users_path, notice: 'User updated successfully.'
+    end
+
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: 'User deleted successfully.'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :encrypted_password, :password_confirmation, :role)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,15 @@ Rails.application.routes.draw do
   resources :surveys
   resources :organizations
 
-
-
   devise_for :users
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root :to => "home#index"
+  get '/users', to: 'users#index'
+
 
 
 end
